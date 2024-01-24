6ede5d6549db34247555ae5295bf968d16454654
Root2308 <140229025+Root2308@users.noreply.github.com>
2024-01-24 23:40:16 +0530

    Add files via upload

---
+++ landing.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Sinhgad Academy of Engineering</title>
+	<link rel="stylesheet" type="text/css" href="s3.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+</head>
+</head>
+<body>
+<header>
+	<nav class="nav1">
+		<div>
+			<p>Sinhgad academy of Engineering &nbsp;
+			   Contact: +91 9876541230
+			</p>
+		</div>
+		<div>
+			<i class="fa fa-facebook-f"></i>
+			<i class="fa fa-instagram"></i>
+			<i class="fa fa-google"></i>
+			<a href="#">Signup</a>
+			<a href="#">Signin</a>
+		</div>
+	</nav>
+
+	<nav class="nav2">
+		<div class="logo">
+			Sinhgad Academy of Engineering
+		</div>
+		<div class="menu">
+			<a href="#">Home</a>
+			<a href="#">Admission</a>
+			<a href="#">Courses</a>
+			<a href="#">Events</a>
+			<a href="#">Pages</a>
+			<a href="#">Student</a>
+			<a href="#">About</a>
+			<a href="#">Contact</a>
+		</div>
+	</nav>
+	<div class="h-txt">
+		<h1>
+			<h1>WELCOME TO <span>SINHGAD ACADEMY OF ENGINEERING</span></h1>
+		</h1>
+		<p>
+			"We are commmitted to produce not only good engineers but good human beings also."
+		</p>
+		<a href="#">Courses</a>
+		<a href="#">Learn More</a>
+	</div>
+</header>
+</body>
+</html>
